ca0fd13cfcfe9d547584b8f764969475ba552ed8
=vinaybhosale <=mailvnay@gmail.com>
2019-05-09 17:01:30 +0530

    Added matplotlib basic problem set

--- Matplotlib/plotting.py
+++ Matplotlib/plotting.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 
 
 class matplotlibBasics:
 
+    # plot a simple graph
     @staticmethod
     def simpleplot():
         plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
@@ -21,6 +23,7 @@ class matplotlibBasics:
         plt.title("Plot By Values")
         plt.show()
 
+    # plotting a graph by taking values from an external file
     @staticmethod
     def plot_values_of_textfile():
 
@@ -28,13 +31,51 @@ class matplotlibBasics:
         with open(file) as f:
             data = f.read()
         data = data.split('\n')
-        x = [row.split(' ')[0] for row in data]
-        y = [row.split(' ')[1] for row in data]
+
+        print("AAAAAAAAA", data)
+        x = [int(row.split(' ')[0]) for row in data]
+        y = [int(row.split(' ')[1]) for row in data]
         plt.plot(x, y)
         plt.xlabel("X-AXIS")
         plt.ylabel("Y-AXIS")
         plt.title("Value from .txt file")
         plt.show()
+
+    #plotting line charts for a range (1 jan to 30 jan) from data stored in csv file
+    @staticmethod
+    def plot_line_chart_of_csv_file():
+        # try:
+            df = pd.read_csv('docdata.csv')
+            print(df)
+            df.plot()
+            plt.show()
+
+        # except(FileNotFoundError):
+        #     print("Code Executed In Catch Block")
+
+    # Plotting two lines in a same plot with legends
+    @staticmethod
+    def plot_two_lines_with_legends():
+        x1 = [10, 20, 30]
+        y1 = [20, 40, 10]
+        # plotting the line 1 points
+        plt.plot(x1, y1, label="line 1")
+        # line 2 points
+        x2 = [10, 20, 30]
+        y2 = [40, 10, 30]
+        # plotting the line 2 points
+        plt.plot(x2, y2, label="line 2")
+        plt.xlabel('x - axis')
+        # Set the y axis label of the current axis.
+        plt.ylabel('y - axis')
+        # Set a title of the current axes.
+        plt.title('Two or more lines on same plot with suitable legends ')
+        # show a legend on the plot
+        # plt.legend()
+        # Display a figure.
+        plt.show()
 # matplotlibBasics.simpleplot()
 # matplotlibBasics.plot_by_values()
-matplotlibBasics.plot_values_of_textfile()
+# matplotlibBasics.plot_values_of_textfile()
+# matplotlibBasics.plot_line_chart_of_csv_file()
+matplotlibBasics.plot_two_lines_with_legends()
