61d49ad48f32431226ea4b853f091b6dd589428a
=vinaybhosale <=mailvnay@gmail.com>
2019-05-16 19:05:07 +0530

    Added collections Code to fetch dataset columns by name

--- LinearRegression/RegressionDataModel.py
+++ LinearRegression/RegressionDataModel.py
@@ -1,7 +1,4 @@
 import collections
 
-class LinearRegressionDataModel:
-    """
-    It's  a Model Prepared to hold all properties which is going to be used further
-    """
-    RegressionModel = collections.namedtuple('Data', ['xTrain', 'xTest', 'yTrain', 'yTest'])
+
+
